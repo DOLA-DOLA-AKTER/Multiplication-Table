516e9622690a1ad27703510307263d905d9a824c
DOLA-DOLA-AKTER <dola.akter20320@gmail.com>
2026-03-29 06:41:19 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 let table = document.querySelector(".table");
-let searchInput = document.querySelector("#search");
 
-function generateTable(filter = "") {
-  table.innerHTML = "";
 
   for (let i = 1; i <= 100; i++) {
 
-    if (filter && i != filter) 
-        continue;
-
     let tr = document.createElement("tr");
-    
-    let lastDigit = i % 10;
-
+          
     // colors
-
-    if (lastDigit === 2) {
+    if (i == 2 || i ==12 || i == 22 || i ==32 ||i == 42 || i ==52 || i == 62 || i == 72 || i == 82 || i == 92) {
         tr.style.backgroundColor = 'rgb(200, 200, 244)';
     }
-    else if (lastDigit === 3) {
+    else if (i == 3 || i ==13 || i == 23 || i ==33 ||i == 43 || i ==53 || i == 63 || i == 73 || i == 83 || i == 93) {
         tr.style.backgroundColor = 'rgb(241, 172, 241)';
     }
-    else if (lastDigit === 4) {
+    else if (i == 4 || i ==14 || i == 24 || i ==34 ||i == 44 || i ==54 || i == 64 || i == 74 || i == 84 || i == 94) {
         tr.style.backgroundColor = 'rgb(203, 237, 203)';
     }
-    else if (lastDigit === 5) {
+    else if (i == 5 || i ==15 || i == 25 || i ==35 ||i == 45 || i ==55 || i == 65 || i == 75 || i == 85 || i == 95) {
         tr.style.backgroundColor = 'rgb(234, 234, 182)';
     }
-    else if (lastDigit === 6) {
+    else if (i == 6 || i ==16 || i == 26 || i ==36 ||i == 46 || i ==56 || i == 66 || i == 76 || i == 86 || i == 96) {
         tr.style.backgroundColor = 'rgb(188, 223, 237)';
     }
-    else if (lastDigit === 7) {
+    else if (i == 7 || i ==17 || i == 27 || i ==37 ||i == 47 || i ==57 || i == 67 || i == 77 || i == 87 || i == 97) {
         tr.style.backgroundColor = 'rgb(241, 194, 194)';
     }
-    else if (lastDigit === 8) {
+    else if (i == 8 || i ==18 || i == 28 || i ==38 ||i == 48 || i ==58 || i == 68 || i == 78 || i == 88 || i == 98) {
         tr.style.backgroundColor = 'rgb(192, 240, 240)';
     }
-    else if (lastDigit === 9) {
+    else if (i == 9 || i ==19 || i == 29 || i ==39 ||i == 49 || i ==59 || i == 69 || i == 79 || i == 89 || i == 99) {
         tr.style.backgroundColor = 'rgb(238, 162, 149)';
     }
-    else if (lastDigit === 0) {
+    else if (i == 0) {
         tr.style.backgroundColor = 'rgb(183, 174, 239)';
     }
 
@@ -60,15 +51,7 @@ function generateTable(filter = "") {
 
     table.appendChild(tr);
   }
-}
 
 generateTable();
 
-// Live Search
-searchInput.addEventListener("input", () => {
-  generateTable(searchInput.value);
-});
 
-
-// if (filter && !i.toString().includes(filter)) 
-//     continue;
